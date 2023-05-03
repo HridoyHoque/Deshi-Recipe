258e5edae4552bb09ca1f00e7223f5389c2a5e8f
HridoyHoque <hhridoy155@gmail.com>
2023-05-03 11:37:46 +0600

    Added Login page

--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import './Login.css'
+
 
 const Login = () => {
     return (
-        <div>
-            <h2>This is login page</h2>
-        </div>
+       <Container className='mx-auto w-50 login-container'>
+         <Form>
+            <h2 className='text-center'>Login Your Account</h2>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+  
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+       </Container>
     );
 };
 
--- src/components/routes/Routes.jsx
+++ src/components/routes/Routes.jsx
@@ -4,6 +4,7 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home ";
 import Blog from "../Pages/Blog/Blog";
 import Login from "../Pages/Login/Login";
+import Registration from "../Pages/Registration/Registration";
 
 
 const router = createBrowserRouter([
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
         {
             path: '/login',
             element: <Login></Login>
+        },
+        {
+            path: '/registration',
+            element: <Registration></Registration>
         }
       ]
     }
