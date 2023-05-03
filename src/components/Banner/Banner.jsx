4b447cf338e21f67c3850a09f595abc56ab2d53c
HridoyHoque <hhridoy155@gmail.com>
2023-05-03 14:44:14 +0600

    Added banner page

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Carousel, Container } from 'react-bootstrap';
+
+const Banner = () => {
+    return (
+        <Container className='rounded-lg'>
+            <Carousel>
+      <Carousel.Item interval={1000}>
+        <img
+         style={{height: "550px"}}
+          className="d-block w-100 object-cover"
+          src="https://i.ibb.co/WFwwVXw/pexels-armin-rimoldi-5553620.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          
+          <h1>Pizza is a beloved dish that has become popular all over the world.</h1>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={500}>
+        <img
+          style={{height: "550px"}}
+          className="d-block w-100 object-cover"
+          src="https://i.ibb.co/0jc9wnm/pexels-mumtahina-tanni-6260921.jpg"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+       
+   <h1>Pizza is a beloved dish that has become popular all over the world.</h1>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+         style={{height: "550px"}}
+          className="d-block w-100 object-cover"
+          src="https://i.ibb.co/rHRpsNN/pexels-roman-odintsov-4551832.jpg"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h1>The flavor of steak can vary depending on the cut, preparation, and seasoning.
+          </h1>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+        </Container>
+    );
+};
+
+export default Banner;
--- src/components/Navbar/Header.jsx
+++ src/components/Navbar/Header.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+
 const Header = () => {
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
       <Container>
-        <Navbar.Brand href="#home">Deshi Recipe</Navbar.Brand>
+        <Link to='/' className='text-decoration-none fw-bold'> <span>Deshi</span> Recipe</Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto gap-3">
-            <Link to='/' className='text-decoration-none text-white'>Home</Link>
-            <Link to='/blog' className='text-decoration-none text-white'>Blog</Link>
+          <Nav className="mx-auto gap-3">
+            <Link to='/' className='text-decoration-none text-black'>Home</Link>
+            <Link to='/blog' className='text-decoration-none text-black'>Blog</Link>
+            <Link to='/' className='text-decoration-none text-black'>Order</Link>
           </Nav>
 
           <Link to='/login'><Button>Login</Button></Link>
