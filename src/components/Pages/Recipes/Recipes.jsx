6ad51bb0934b77278b6e07a22dec19a48d784062
HridoyHoque <hhridoy155@gmail.com>
2023-05-04 16:27:40 +0600

    implemented chef details in recipe page

--- src/components/Pages/Recipes/Recipes.jsx
+++ src/components/Pages/Recipes/Recipes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Image } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
+import { RiHeart2Line } from "react-icons/ri";
 
 const Recipes = () => {
 
@@ -9,7 +10,18 @@ const Recipes = () => {
     console.log(chef);
     return (
         <Container>
-            
+            <div className='text-center mt-3'>
+                <Image width="250" height="250" src={chef.chefPicture} roundedCircle />
+            </div>
+            <div className='d-flex text-center align-items-center'>
+                <h1 className='mt-2 mx-auto ps-5'>Name: {chef.chefName}</h1 >
+                <span className='ps-5'><RiHeart2Line />
+                </span> {chef.likes}
+            </div>
+                <h3 className='text-center'>About: {chef.description}</h3 >
+               <h5 className='text-center text-primary'>I have {chef. numberOfRecipes} of Recipes</h5>
+               <h6 className='text-center text-success'>I have {chef.yearsOfExperience} Years of Cooking Experience</h6>
+               <hr />
         </Container>
     );
 };
