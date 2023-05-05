b1047cf5d7af9f2fa2a270b808a2cc3cdab10aa6
HridoyHoque <hhridoy155@gmail.com>
2023-05-05 22:30:11 +0600

    implementet lazyLoad

--- src/components/Chefs/Chef.jsx
+++ src/components/Chefs/Chef.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { RiHeart2Line } from "react-icons/ri";
+import LazyLoad from 'react-lazyload';
 
 const Chef = ({ chef }) => {
     const {id, chefPicture, chefName , yearsOfExperience, description, numberOfRecipes, likes} = chef;
     return (
         <div className='mb-2 ms-sm-1'> 
             <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={chefPicture} />
+                <figure>
+                    <LazyLoad
+                    className="LazyLoad"
+                    width={400}
+                    height={200}
+                    offset={300}
+                    threshold={0.75}
+                    ><Card.Img variant="top" src={chefPicture} /></LazyLoad>
+                </figure>
+           
             <Card.Body>
                 <div className='d-flex'><Card.Title className='me-auto '>{chefName}
                 
